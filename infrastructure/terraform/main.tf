11a605d48368990dcc381863dfe10c9c06994852
timoangerer <timo.angerer@code.berlin>
2023-11-19 21:52:20 +0100

    Add basic terraform setup for pulsar

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    pulsar = {
+      source  = "streamnative/pulsar"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "pulsar" {
+  web_service_url = "http://127.0.0.1:51195"
+}
+
+resource "pulsar_tenant" "tenant" {
+  tenant           = "rocky"
+  allowed_clusters = ["pulsar-mini"]
+}
+
+resource "pulsar_namespace" "gen" {
+  tenant    = pulsar_tenant.tenant.tenant
+  namespace = "gen"
+}
+
+variable "topics" {
+  description = "List of topic names"
+  type        = list(string)
+  default     = ["requested_txt2img_generation", "dlq_requested_txt2img_generation", "completed_txt2img_generation"]
+}
+
+resource "pulsar_topic" "topics" {
+  for_each   = toset(var.topics)
+  topic_name = each.value
+  tenant     = pulsar_tenant.tenant.tenant
+  namespace  = pulsar_namespace.gen.namespace
+  partitions = 0
+  topic_type = "persistent"
+}
