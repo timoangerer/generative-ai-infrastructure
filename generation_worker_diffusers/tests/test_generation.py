474d1516e1d7bca74b32ae4ad7e74e1a45b64ba1
timoangerer <timo.angerer@code.berlin>
2023-12-02 10:02:12 +0100

    Add basic image generation test case

--- generation_worker_diffusers/tests/test_generation.py
+++ generation_worker_diffusers/tests/test_generation.py
@@ -1,25 +1,41 @@
-from pathlib import Path
+import datetime
+import os
 
-from sd_generation import (GenerationSettings, generate_text2image,
-                           get_model_path_by_name)
+from src.settings import get_settings
+from src.stable_diffusion import GenerationSettings, generate_text2image
+from src.utils import get_model_path_by_name
 
 
 def test_generate_text2image():
-    models_file = Path("models/models.json")
+    os.environ['MODELS_PATH'] = './models'
+
+    settings = get_settings()
+
+    models_path = settings.models_path
+
     model_path = get_model_path_by_name(
-        "v1-5-pruned-emaonly", json_file=str(models_file.absolute()))
+        model_name="v1-5-pruned-emaonly", root_folder=models_path, json_file="models.json")
 
     settings = GenerationSettings(
-        prompt="a cat",
+        prompt="A painting of a cat",
         negative_prompt="",
         width=512,
         height=512,
-        guidance_scale=7.0,
+        guidance_scale=7.5,
         num_inference_steps=5,
-        sampler_name="sampler_name",
-        model_path="/Users/timoangerer/devel/generative-ai-infrastructure/generation_worker_sidecar/models/stable-diffusion/AbyssOrangeMix.safetensors"
+        sampler_name="ddim",
+        seed=42
     )
 
-    img = generate_text2image(settings)
+    def generation_progress_callback(self, step: int, timestep: int, callback_kwargs: dict):
+        print(f"Step: {step}, Timestep: {timestep}")
+        return callback_kwargs
 
-    img.save("test.png")
+    img = generate_text2image(
+        settings=settings, model_path=model_path, callback_on_step_end=generation_progress_callback)
+
+    assert img is not None
+
+    current_time = datetime.datetime.now().isoformat()
+    file_name = f"output/{current_time}.png"
+    img.save(file_name)
