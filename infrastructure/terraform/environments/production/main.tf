cba79da94f0e7c3f234bc4e684d901bb74a62c37
timoangerer <timo.angerer@code.berlin>
2024-01-16 09:35:09 +0000

    Remove efs driver helm deployment

--- infrastructure/terraform/environments/production/main.tf
+++ infrastructure/terraform/environments/production/main.tf
@@ -54,14 +54,6 @@ provider "kubernetes" {
   }
 }
 
-resource "helm_release" "aws_efs_csi_driver" {
-  name       = "aws-efs-csi-driver"
-  namespace  = "kube-system"
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-  chart      = "aws-efs-csi-driver"
-  version    = "2.5.3"
-}
-
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = var.namespace
