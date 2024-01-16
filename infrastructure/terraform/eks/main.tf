2ea55b93ad8685565a344b7a5e7233fbb0ab0191
timoangerer <timo.angerer@code.berlin>
2024-01-16 17:29:39 +0000

    Increase disk size of default node group

--- infrastructure/terraform/eks/main.tf
+++ infrastructure/terraform/eks/main.tf
@@ -123,14 +123,28 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    general = {
-      name = "node-group-general"
+    general_node_group = {
+      name = "general_node_group"
 
       instance_types = ["t3.xlarge"]
 
       min_size     = 1
       max_size     = 4
       desired_size = 3
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = 30
+            volume_type           = "gp3"
+            iops                  = 1000
+            throughput            = 125
+            encrypted             = false
+            delete_on_termination = true
+          }
+        }
+      }
     }
   }
 }
@@ -153,4 +167,31 @@ resource "aws_eks_addon" "codedns" {
 resource "aws_eks_addon" "efs-csi" {
   cluster_name = module.eks.cluster_name
   addon_name   = "aws-efs-csi-driver"
+}
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
 }
