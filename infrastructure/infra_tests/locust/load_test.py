fe37d7fcb3f7559a6433d13574c974542c5743d2
timoangerer <timo.angerer@code.berlin>
2024-01-22 17:06:13 +0000

    Implement basic load test

---
+++ infrastructure/infra_tests/locust/load_test.py
@@ -0,0 +1,69 @@
+import random
+from locust import HttpUser, task, between, run_single_user, events
+import time
+
+class ApiUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "http://localhost:8000"
+
+    @task
+    def post_and_poll(self):
+        random_seed = random.randint(0, 10000)
+
+        post_data = {
+            "generation_settings": {
+                "prompt": "A dog",
+                "negative_prompt": "",
+                "styles": [],
+                "seed": random_seed,
+                "sampler_name": "asdf",
+                "batch_size": 1,
+                "n_iters": 1,
+                "steps": 5,
+                "cfg_scale": 7.0,
+                "width": 512,
+                "height": 512,
+                "override_settings": {
+                    "sd_model_checkpoint": "cc6cb27"
+                }
+            },
+            "metadata": {
+                "grouping": "test/grouping"
+            }
+        }
+
+        start_time = time.time()  # Record the start time of the POST request
+
+        with self.client.post("/images", json=post_data, catch_response=True) as post_response:
+            if post_response.status_code == 200:
+                image_id = post_response.text.strip('"')
+                
+                while True:
+                    with self.client.get(f"/images/{image_id}", name="/images/[id]", catch_response=True) as get_response:
+                        if get_response.status_code == 200:
+                            image_data = get_response.json()
+                            if image_data.get("image_url"):
+                                # Calculate total request time
+                                total_time = int((time.time() - start_time) * 1000)
+                                
+                                # Fire a custom event to log the total time
+                                events.request.fire(
+                                    request_type="CUSTOM",
+                                    name="Total time for image generation",
+                                    response_time=total_time,
+                                    response_length=0,
+                                    exception=None,
+                                )
+                                
+                                get_response.success()
+                                return
+                        else:
+                            get_response.failure(f"Unexpected status code: {get_response.status_code}")
+                        
+                        time.sleep(1)
+            else:
+                post_response.failure(f"Failed to start image generation: {post_response.status_code}")
+
+# if launched directly, e.g. "python3 debugging.py", not "locust -f debugging.py"
+if __name__ == "__main__":
+    run_single_user(ApiUser)
